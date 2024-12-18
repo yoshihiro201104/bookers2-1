22706d045606f08422a4e63c248ace7dea33a464
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-18 12:48:57 +0000

    [Add]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,27 @@ class BooksController < ApplicationController
 
     # 投稿データの保存
     def create
-      @book = book.new(book_params)
+      @book = Book.new(book_params)
       @book.user_id = current_user.id
       @book.save
-      redirect_to book_path
+      redirect_to book_path(@book.id)
     end
 
+    def show
+      @book = Book.find(params[:id])
+      # @profile_image = @user.get_profile_image(100, 100) 
+      @user = @book.user
+
+    end
+
+    def index
+      @books = Book.all
+    end
+    
+    private
+
+    def book_params
+      params.require(:book).permit(:title, :body, :user_id)
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,14 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @user = current_user
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @profile_image = @user.get_profile_image(100, 100) 
+    @book = Book.new
   end
 
   def edit
